9b5df1854b8cc3455090ec1e1bfa66b64e0ba667
lauraprem <premillieu.laura@gmail.com>
2014-05-07 10:55:08 +0200

    Création des packages

--- Tetris/src/tetris/Modele/JeuDeTetris.java
+++ Tetris/src/tetris/Modele/JeuDeTetris.java
@@ -4,19 +4,12 @@
  * and open the template in the editor.
  */
 
-package tetris;
+package tetris.Modele;
 
 /**
  *
  * @author p1307999
  */
-public class Tetris {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class JeuDeTetris {
     
 }
