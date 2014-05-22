b21c1563231dbdcd04f748ac821e71e0df3d1fdd
lauraprem <premillieu.laura@gmail.com>
2014-05-22 16:16:33 +0200

    Vue : keylistener

--- Tetris/src/tetris/Vue/VuePrincipale.java
+++ Tetris/src/tetris/Vue/VuePrincipale.java
@@ -10,6 +10,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.BorderFactory;
@@ -31,12 +33,16 @@ public class VuePrincipale extends JFrame {
     private final int LARGEUR = 1000;
     private final int LARGEUR_PANEL_COTE = 250;
     private final int LARGEUR_PANEL_CENTRE = 500;
+    private final int NB_CASE_LIGNE = 10;
+    private final int NB_CASE_COLONE = 20;
 
     public VuePrincipale() {
         super();
 
         this.setSize(new Dimension(LARGEUR, HAUTEUR));
         this.setResizable(false);
+        this.setFocusable(false);
+
         build();
 
         addWindowListener(new WindowAdapter() {
@@ -52,6 +58,7 @@ public class VuePrincipale extends JFrame {
     private void build() {
         JComponent panelPrincipal = new JPanel(new BorderLayout());
         panelPrincipal.setBackground(Color.BLACK);
+        panelPrincipal.setFocusable(true);
         this.add(panelPrincipal);
 
         JMenuBar jm = new JMenuBar();
@@ -60,7 +67,7 @@ public class VuePrincipale extends JFrame {
         panelPrincipal.add(jm, BorderLayout.PAGE_START);
         setJMenuBar(jm);
 
-         //Panel de score
+        //Panel de score
         JComponent panelScore = new JPanel(new BorderLayout());
         panelScore.setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 1));
         panelScore.setPreferredSize(new Dimension(LARGEUR_PANEL_COTE, HAUTEUR));
@@ -73,29 +80,32 @@ public class VuePrincipale extends JFrame {
         panelGrille.setPreferredSize(new Dimension(LARGEUR_PANEL_CENTRE, HAUTEUR));
         panelGrille.setOpaque(false);
         panelPrincipal.add(panelGrille, BorderLayout.CENTER);
-        
+
         //Cases de la grille
-        for (int i = 1; i<= 200; i++)
-        {
+        for (int i = 1; i <= NB_CASE_COLONE * NB_CASE_LIGNE; i++) {
             JPanel macase = new JPanel();
             macase.setOpaque(false);
             macase.setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 1));
             macase.setPreferredSize(new Dimension(42, 42));
             GridBagConstraints c = new GridBagConstraints();
-            if(i%10 == 0 )
-            {c.gridwidth =  GridBagConstraints.REMAINDER;}
+            if (i % 10 == 0) {
+                c.gridwidth = GridBagConstraints.REMAINDER;
+            }
             //macase.set
-            panelGrille.add(macase,c);
-            
+            panelGrille.add(macase, c);
+
         }
-        
-        
+
         //panel des pièces suivantes
         JComponent panelPieceSuivante = new JPanel(new BorderLayout());
         panelPieceSuivante.setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 1));
-        panelPieceSuivante.setPreferredSize(new Dimension(LARGEUR_PANEL_COTE ,HAUTEUR ));
+        panelPieceSuivante.setPreferredSize(new Dimension(LARGEUR_PANEL_COTE, HAUTEUR));
         panelPieceSuivante.setOpaque(false);
         panelPrincipal.add(panelPieceSuivante, BorderLayout.LINE_END);
 
+        panelPrincipal.addKeyListener(new Clavier());
+
     }
+
+
 }
