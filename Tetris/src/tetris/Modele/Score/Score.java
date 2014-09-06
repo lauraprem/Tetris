8e45777e6a7ddbdbe76a39207fcbb2b9b3c4b24e
FaCoInf <FaCoInf@users.noreply.github.com>
2014-09-06 14:05:54 +0200

    Update Score.java

--- Tetris/src/tetris/Modele/Score/Score.java
+++ Tetris/src/tetris/Modele/Score/Score.java
@@ -3,7 +3,7 @@ package tetris.Modele.Score;
 /**
  * Représente un <b>Score</b>
  *
- * @author Corinne Fagno && Laura Prémillieu
+ * @author Corinne && Laura Prémillieu
  */
 public class Score {
     private int point;
