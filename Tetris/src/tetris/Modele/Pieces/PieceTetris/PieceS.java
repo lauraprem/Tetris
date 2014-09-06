52ae1ba62bf694771a6c48b48a5d5093d808c033
FaCoInf <FaCoInf@users.noreply.github.com>
2014-09-06 14:03:33 +0200

    Update PieceS.java

--- Tetris/src/tetris/Modele/Pieces/PieceTetris/PieceS.java
+++ Tetris/src/tetris/Modele/Pieces/PieceTetris/PieceS.java
@@ -8,7 +8,7 @@ import tetris.Modele.Pieces.Position;
 /**
  * Représente la <b>Piece de Tetris S</b>
  *
- * @author Corinne Fagno && Laura Prémillieu
+ * @author Corinne && Laura Prémillieu
  */
 public class PieceS extends PieceDeTetris {
     // CONSTRUCTEUR
